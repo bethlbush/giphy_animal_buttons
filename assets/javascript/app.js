059ebd0065cb0a76694165c047ed2a7c69e93226
Beth Bush <Beth Bush>
2017-03-11 08:21:53 -0500

    changed to https for heroku

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -53,7 +53,7 @@ console.log(topics);
 
 
         //set up the query to send to giphy.com
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
         //AJAX GET request
